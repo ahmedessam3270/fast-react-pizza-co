c70de26dc705ed8b4708b43cfffc16c1f599b14d
ahmed <ahmedessam201790@gmail.com>
2024-01-11 22:40:42 +0200

    finishing tailwind sytling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,11 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
